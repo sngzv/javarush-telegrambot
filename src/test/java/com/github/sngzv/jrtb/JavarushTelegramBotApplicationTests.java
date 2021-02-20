82faf0cfd1974f1f3d0a85a22e04f88bd1fa2066
Dima Sngzv <dsineguzov@gmail.com>
2021-02-20 19:56:28 +0300

    JRTB-0: added SpringBoot skeleton to the project.

---
+++ src/test/java/com/github/sngzv/jrtb/JavarushTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sngzv.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
